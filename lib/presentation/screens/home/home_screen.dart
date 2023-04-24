24d18d5bcd29d429dac4fa76048530029fd7faa4
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-24 15:03:23 -0500

    Menu Items implementados en HomeScreen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,46 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    appMenuItems;
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        final menuItem = appMenuItems[index];
+        return _ItemMenu(menuItem: menuItem,);
+     },);
+  }
+}
+
+class _ItemMenu extends StatelessWidget {
+
+  final MenuItem menuItem;
+
+  const _ItemMenu({ required this.menuItem});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      onTap: (){
+        //TODO: Navegar a otras pantallas
+      },
     );
   }
 }
