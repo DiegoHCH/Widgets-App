e0c42f72d1627cfad11af48810fb08d0c00b7b02
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-24 14:37:53 -0500

    Creacion de menu items, estilo del appbar y homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
    runApp(const MyApp());
@@ -14,11 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Widgets',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 0).getTheme(),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hola desde Widgets'),
-        ),
-      ),
+      home: const HomeScreen()
     );
   }
 }
