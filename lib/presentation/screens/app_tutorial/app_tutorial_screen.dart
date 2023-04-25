1ed7ecc4545685e71cb6aad734798fea8f06a9d9
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-24 20:20:34 -0500

    Screens Creadas y Archivo de barril actualizado

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TutorialScreen extends StatelessWidget {
+
+  static const name = 'tutorial';
+
+  const TutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UiControlScreen extends StatelessWidget {
+
+  static const name = 'ui_control';
+
+  const UiControlScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
