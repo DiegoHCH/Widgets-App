f33710b4b91ec0562a8a7eda7842a9fe39dadb20
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-24 22:39:39 -0500

    Inicializacion de PageView y Creaciond e Slides

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+
+}
+
+
+final slides = <SlideInfo> [
+  SlideInfo('Busca la comida', 'Ea do excepteur officia minim aute.', 'assets/images/1.png'),
+  SlideInfo('Entrega Rapida', 'Labore amet occaecat in ad proident deserunt exercitation est sit.', 'assets/images/2.png'),
+  SlideInfo('Disfruta la comida', 'Cillum aute irure nulla reprehenderit id ex eu irure aute nulla.', 'assets/images/3.png'),
+];
+
 class TutorialScreen extends StatelessWidget {
 
   static const name = 'tutorial';
 
   const TutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map(
+          (slideData) => _Slide(slideData.title, slideData.caption, slideData.imageUrl)
+        ).toList()
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide(this.title, this.caption, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
