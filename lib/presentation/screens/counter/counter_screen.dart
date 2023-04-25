0d77ef1d4b288b88201130d42aef1b76ba9fcf18
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-25 09:51:00 -0500

    Implementacion de paquete flutter_riverpod y creacion de provider

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
 
   static const name = 'riverpod';
 
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final int clickCounter = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod Gestor de Estados'),
       ),
       body: Center(
-        child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge,),
+        child: Text('Valor: $clickCounter', style: Theme.of(context).textTheme.titleLarge,),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
