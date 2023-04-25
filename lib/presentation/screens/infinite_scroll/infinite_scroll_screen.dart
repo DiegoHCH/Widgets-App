854a4c57342737dec5c1af375e054afc9a04eb55
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-24 23:44:00 -0500

    Finalizacion Infinite Scroll

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -48,6 +48,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     isLoading = false;
     if ( !isMounted ) return;
     setState(() {});
+    moveScrollToBottom();
   }
 
   Future<void> onRefresh() async {
@@ -63,6 +64,16 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     addFiveImages();
     
     setState(() {});
+  }
+
+  void moveScrollToBottom() {
+    if ( scrollController.position.pixels + 150 <= scrollController.position.maxScrollExtent ) return;
+
+    scrollController.animateTo(
+      scrollController.position.pixels + 120, 
+      duration: const Duration(milliseconds: 300), 
+      curve: Curves.fastOutSlowIn
+    );
   }
 
   void addFiveImages() {
